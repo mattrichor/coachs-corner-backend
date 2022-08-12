2b803f96b4b8b4d0a619094f2268b203474a3c25
Matt Russelli <russellimatt@gmail.com>
2022-08-12 18:35:22 -0400

    changed routes for register player and coach in express to match react

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/AuthController')
 const middleware = require('../middleware')
 
 router.post('/login', controller.Login)
-router.post('/register', controller.Register)
+router.post('/register/coach', controller.Register)
 router.post(
   '/update',
   middleware.stripToken,
