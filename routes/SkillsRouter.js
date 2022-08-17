a847005b494055c94d41995e72c2d09b61365e93
mattrichor <matt.a.geyer@gmail.com>
2022-08-17 19:57:05 -0400

    thinkin i found the bug... we will see

--- routes/SkillsRouter.js
+++ routes/SkillsRouter.js
@@ -4,7 +4,7 @@ const middleware = require('../middleware')
 
 router.get('/:player_id', controller.GetSkillsByPlayerId)
 router.get('/:player_id/:workout_id', controller.GetAndUpdateSkillLevel)
-router.get('/:skill_id', controller.GetSkillName)
+router.get('/ids/:skill_id', controller.GetSkillName)
 
 //this is a comment
 module.exports = router
