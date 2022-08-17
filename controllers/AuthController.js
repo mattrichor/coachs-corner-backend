95d99dfd671eb406619893525ebf2e9f7f42db8c
mattrichor <matt.a.geyer@gmail.com>
2022-08-17 10:51:04 -0400

    readded player login functionality... not sure why it disappeared lol

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,7 @@
-const { Coach } = require('../models')
+const { Coach, Player, Skill } = require('../models')
 const middleware = require('../middleware')
 
-const Login = async (req, res) => {
+const LoginCoach = async (req, res) => {
   try {
     const coach = await Coach.findOne({
       where: { email: req.body.email },
@@ -30,7 +30,40 @@ const Login = async (req, res) => {
   }
 }
 
-const Register = async (req, res) => {
+const LoginPlayer = async (req, res) => {
+  try {
+    const player = await Player.findOne({
+      where: { email: req.body.email },
+      raw: true
+    })
+    if (
+      player &&
+      (await middleware.comparePassword(
+        player.passwordDigest,
+        req.body.password
+      ))
+    ) {
+      let payload = {
+        id: player.id,
+        email: player.email,
+        name: player.name,
+        primaryPosition: player.primaryPosition,
+        secondaryPosition: player.secondaryPosition,
+        height: player.height,
+        weight: player.weight,
+        age: player.age,
+        coachId: player.coachId
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ player: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    throw error
+  }
+}
+
+const RegisterCoach = async (req, res) => {
   try {
     const { name, email, password, sport, teamName } = req.body
     let passwordDigest = await middleware.hashPassword(password)
@@ -42,6 +75,69 @@ const Register = async (req, res) => {
       teamName
     })
     res.send(coach)
+  } catch (error) {
+    throw error
+  }
+}
+
+const RegisterPlayer = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      primaryPosition,
+      secondaryPosition,
+      height,
+      weight,
+      age,
+      coachId
+    } = req.body
+    let passwordDigest = await middleware.hashPassword(password)
+    const player = await Player.create({
+      name,
+      email,
+      passwordDigest,
+      primaryPosition,
+      secondaryPosition,
+      height,
+      weight,
+      age,
+      coachId
+    })
+    res.send(player)
+    const skills = await Skill.bulkCreate([
+      {
+        skillName: 'Speed',
+        skillLevel: 25,
+        playerId: player.id
+      },
+      {
+        skillName: 'Catching',
+        skillLevel: 25,
+        playerId: player.id
+      },
+      {
+        skillName: 'Pitch Control',
+        skillLevel: 25,
+        playerId: player.id
+      },
+      {
+        skillName: 'Pitch Velocity',
+        skillLevel: 25,
+        playerId: player.id
+      },
+      {
+        skillName: 'Contact',
+        skillLevel: 25,
+        playerId: player.id
+      },
+      {
+        skillName: 'Power',
+        skillLevel: 25,
+        playerId: player.id
+      }
+    ])
   } catch (error) {
     throw error
   }
@@ -72,8 +168,10 @@ const CheckSession = async (req, res) => {
 }
 
 module.exports = {
-  Login,
-  Register,
+  LoginCoach,
+  LoginPlayer,
+  RegisterCoach,
+  RegisterPlayer,
   UpdatePassword,
   CheckSession
 }
--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 const controller = require('../controllers/AuthController')
 const middleware = require('../middleware')
 
-router.post('/login', controller.Login)
-router.post('/register/coach', controller.Register)
+router.post('/login/coach', controller.LoginCoach)
+router.post('/login/player', controller.LoginPlayer)
+router.post('/register/coach', controller.RegisterCoach)
+router.post('/register/player', controller.RegisterPlayer)
 router.post(
   '/update',
   middleware.stripToken,
