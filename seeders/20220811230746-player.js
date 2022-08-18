d66a3867fc0177c258ce23ff0480cda05eac30e2
Richard Remley <5rremley@gmail.com>
2022-08-18 16:37:41 -0500

    fixed pictures

--- seeders/20220811230746-player.js
+++ seeders/20220811230746-player.js
@@ -1,6 +1,6 @@
-'use strict'
-const falso = require('@ngneat/falso')
-const { Coach, sequelize } = require('../models')
+"use strict";
+const falso = require("@ngneat/falso");
+const { Coach, sequelize } = require("../models");
 
 // positions: pitcher, catcher, first baseman, second baseman, third baseman, shortstop, left fielder, center fielder and right fielder (9)
 
@@ -124,221 +124,221 @@ module.exports = {
     //   })
     // )
 
-    return queryInterface.bulkInsert('players', [
+    return queryInterface.bulkInsert("players", [
       {
         name: `Jason Kendricks`,
         email: `jkendricks@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://www.gannett-cdn.com/presto/2021/04/12/NAAS/d6e39359-eac3-467d-a23e-ff82a5e4158d-starkey.JPG?width=660&height=450&fit=crop&format=pjpg&auto=webp',
+          "https://www.gannett-cdn.com/presto/2021/04/12/NAAS/d6e39359-eac3-467d-a23e-ff82a5e4158d-starkey.JPG?width=660&height=450&fit=crop&format=pjpg&auto=webp",
         primaryPosition: `Catcher`,
         secondaryPosition: `First Baseman`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Josh Williams`,
         email: `jwilliams@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://cdn1.sportngin.com/attachments/news_article/b03c-176320943/DSC_0203.jpg',
+          "https://cdn1.sportngin.com/attachments/news_article/b03c-176320943/DSC_0203.jpg",
         primaryPosition: `First Baseman`,
         secondaryPosition: `Second Baseman`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Olivar Rodriguez`,
         email: `orodriguez@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://2qibqm39xjt6q46gf1rwo2g1-wpengine.netdna-ssl.com/wp-content/uploads/2021/05/25300167_web1_L1A-Dominic-Hellman-EDH-052621-FS-1024x686.jpg',
+          "https://2qibqm39xjt6q46gf1rwo2g1-wpengine.netdna-ssl.com/wp-content/uploads/2021/05/25300167_web1_L1A-Dominic-Hellman-EDH-052621-FS-1024x686.jpg",
         primaryPosition: `Second Baseman`,
         secondaryPosition: `Shortstop`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Sunday Walters`,
         email: `swalters@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://www.masslive.com/resizer/1nMmrpOKi8aSAObi_uD3OBk21B4=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/7BHPAXE3ZJAANMYDAZ2FZYWEAQ.JPG',
+          "https://www.masslive.com/resizer/1nMmrpOKi8aSAObi_uD3OBk21B4=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/7BHPAXE3ZJAANMYDAZ2FZYWEAQ.JPG",
         primaryPosition: `Shortstop`,
         secondaryPosition: `Second Baseman`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Robin Roberts`,
         email: `rrobers@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://www.archbishopcurley.org/wp-content/uploads/bfi_thumb/baseball-2400-1260x680-paejoemfqdopmhbtnqvqhz3gin08xcbbm8prrxpjtc.jpg',
+          "https://www.archbishopcurley.org/wp-content/uploads/bfi_thumb/baseball-2400-1260x680-paejoemfqdopmhbtnqvqhz3gin08xcbbm8prrxpjtc.jpg",
         primaryPosition: `Pitcher`,
         secondaryPosition: `First Baseman`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Julian Ortiz`,
         email: `jortiz@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://www.gannett-cdn.com/presto/2019/05/29/PLOU/ab3af271-8cc9-4d40-af9a-1a0e4c22a557-7thRegionTrinityStX_TDE010.jpg',
+          "https://www.al.com/resizer/mV7tSAlEyQ_6OoZeBqMkz-vOJGs=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/ESHL4RBQ4JDLVIRSZAKENQM3EU.JPG",
         primaryPosition: `Third Baseman`,
         secondaryPosition: `First Baseman`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Feliz Ortega`,
         email: `fortega@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://thumbor.bigedition.com/img/Qv2dtYhDxZQxlZYRsLSlJojgRoQ=/800x800/filters:quality(80)/granite-web-prod/22/7c/227cafcb21d7467185c6ab34a129b8ad.jpeg',
+          "https://thumbor.bigedition.com/img/Qv2dtYhDxZQxlZYRsLSlJojgRoQ=/800x800/filters:quality(80)/granite-web-prod/22/7c/227cafcb21d7467185c6ab34a129b8ad.jpeg",
         primaryPosition: `Left Fielder`,
         secondaryPosition: `Center Fielder`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Kent Suzuki`,
         email: `ksuzuki@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'http://www.baseballasia.org/BFA/include/UploadImages/6323489725c209fb8db743-p01.png',
+          "http://www.baseballasia.org/BFA/include/UploadImages/6323489725c209fb8db743-p01.png",
         primaryPosition: `Center Fielder`,
         secondaryPosition: `Left Fielder`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Ken Sisko`,
         email: `jkendricks@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://www.kansas.com/latest-news/2ejq37/picture251836043/alternates/FREE_1140/elliot.jpeg',
+          "https://www.kansas.com/latest-news/2ejq37/picture251836043/alternates/FREE_1140/elliot.jpeg",
         primaryPosition: `Right Fielder`,
         secondaryPosition: `Center Fielder`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Bartolo Rieviera`,
         email: `brieviera@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://dmn-dallas-news-prod.cdn.arcpublishing.com/resizer/nFWrJdV_m9HB5g3mK84rmvdYd9I=/1660x934/filters:focal(2448x786:2458x776):no_upscale()/cloudfront-us-east-1.images.arcpublishing.com/dmn/I6VAHAT7FJGCROWHKXJBBFIDGU.jpg',
+          "https://dmn-dallas-news-prod.cdn.arcpublishing.com/resizer/nFWrJdV_m9HB5g3mK84rmvdYd9I=/1660x934/filters:focal(2448x786:2458x776):no_upscale()/cloudfront-us-east-1.images.arcpublishing.com/dmn/I6VAHAT7FJGCROWHKXJBBFIDGU.jpg",
         primaryPosition: `Shortstop`,
         secondaryPosition: `Left Fielder`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Eddie Murray`,
         email: `emurray@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://www.tvinsider.com/wp-content/uploads/2021/02/COMING_TO_AMERICA_2-1014x570.jpg',
+          "https://www.tvinsider.com/wp-content/uploads/2021/02/COMING_TO_AMERICA_2-1014x570.jpg",
         primaryPosition: `Third Baseman`,
         secondaryPosition: `First Baseman`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Carlos de Silva`,
         email: `desilva@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://www.mlive.com/resizer/M9DkTnhnjS-X8K6ilfVRV6v7P9Q=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/3NN3YJIIRFELFAWWJEKYM73WWQ.JPG',
+          "https://www.mlive.com/resizer/M9DkTnhnjS-X8K6ilfVRV6v7P9Q=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/3NN3YJIIRFELFAWWJEKYM73WWQ.JPG",
         primaryPosition: `Second Baseman`,
         secondaryPosition: `Catcher`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Ryne Sandberg`,
         email: `rsandberg@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://imengine.prod.srp.navigacloud.com/?uuid=1A48F954-78F4-40AA-B79E-E7C0FE510219&type=primary&q=72&width=1024',
+          "https://imengine.prod.srp.navigacloud.com/?uuid=1A48F954-78F4-40AA-B79E-E7C0FE510219&type=primary&q=72&width=1024",
         primaryPosition: `Left Fielder`,
         secondaryPosition: `Center Fielder`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
-        name: `Bartolo Rieviera`,
-        email: `brieviera@gmail.com`,
+        name: `Mike Hanha`,
+        email: `mhanha@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://www.gannett-cdn.com/presto/2021/06/20/PLOU/b23f17d2-e020-45af-8439-43d0f7470e05-21061906-19-2021TrinityvsMcCrackenCountyKHSAABaseball0025MCseq.JPG',
+          "https://www.gannett-cdn.com/presto/2021/04/13/PCIN/747f9fe1-aa33-48cf-9b7c-6089b7cdc407-le2.JPG",
         primaryPosition: `Pitcher`,
         secondaryPosition: `First Baseman`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
+        coachId: 1,
       },
       {
         name: `Ed Walsh`,
         email: `ewalsh@gmail.com`,
         passwordDigest: `1234`,
         proPic:
-          'https://dmn-dallas-news-prod.cdn.arcpublishing.com/resizer/gJbGrugPzcdRZYVZ2ILvIeOZ124=/1660x934/filters:focal(1108x287:1118x277):no_upscale()/cloudfront-us-east-1.images.arcpublishing.com/dmn/SNPCAE6T5RCVDC7PJSLKQYC5UI.jpg',
+          "https://dmn-dallas-news-prod.cdn.arcpublishing.com/resizer/gJbGrugPzcdRZYVZ2ILvIeOZ124=/1660x934/filters:focal(1108x287:1118x277):no_upscale()/cloudfront-us-east-1.images.arcpublishing.com/dmn/SNPCAE6T5RCVDC7PJSLKQYC5UI.jpg",
         primaryPosition: `Catcher`,
         secondaryPosition: `First Baseman`,
         height: falso.randNumber({ min: 50, max: 70 }),
         weight: falso.randNumber({ min: 110, max: 200 }),
         age: falso.randNumber({ min: 12, max: 16 }),
         isActive: falso.randBoolean(),
-        coachId: 1
-      }
-    ])
+        coachId: 1,
+      },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('players', null, {})
-  }
-}
+    return queryInterface.bulkDelete("players", null, {});
+  },
+};
